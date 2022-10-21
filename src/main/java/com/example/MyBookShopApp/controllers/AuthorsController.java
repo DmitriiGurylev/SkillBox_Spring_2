8bff699ac56c2123476fe869b289403c317b9e68
Dmitrii Gurylev <gurylev.dmitrii@mail.ru>
2022-10-21 10:46:27 +0600

    authors added [3]

--- src/main/java/com/example/MyBookShopApp/controllers/AuthorsController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/AuthorsController.java
@@ -1,11 +1,17 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.data.Author;
 import com.example.MyBookShopApp.data.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping
@@ -16,7 +22,25 @@ public class AuthorsController {
 
     @GetMapping("/authors")
     public String authorsPage(Model model){
-        model.addAttribute("authorData", bookService.getAuthorsData());
+//        model.addAttribute("authorData", bookService.getAuthorsData());
+
+        Map<Character, LinkedList<Author>> firstLetterAuthors = new HashMap<>();
+        bookService.getAuthorsData()
+                .forEach(ad -> {
+                    char ch = ad.getName().charAt(0);
+                    LinkedList<Author> ll;
+                    if (firstLetterAuthors.containsKey(ch)) {
+                        ll = firstLetterAuthors.get(ch);
+                        ll.add(ad);
+                    } else {
+                        ll = new LinkedList<>();
+                        ll.add(ad);
+                        firstLetterAuthors.put(ch, ll);
+                    }
+                });
+        model.addAttribute("authorData", firstLetterAuthors);
+
+
         return "authors/index";
-    }
+}
 }
