0831e066b01a27cd9502d7d8a902284d4401d498
Dmitrii Gurylev <gurylev.dmitrii@mail.ru>
2022-10-23 20:09:39 +0600

    errors fixed

--- src/main/java/com/example/MyBookShopApp/data/BookService.java
+++ src/main/java/com/example/MyBookShopApp/data/BookService.java
@@ -20,16 +20,16 @@ public class BookService {
 
     public List<Book> getBooksData() {
         List<Book> books = jdbcTemplate.query(
-                "SELECT books.id, books.title, books.priceOld, books.price, authors.name AS author " +
+                "SELECT books.id, books.title, books.PRICE_OLD, books.price, authors.first_name AS authorFirstName, authors.first_name AS authorLastName " +
                         "FROM books " +
                         "INNER JOIN authors " +
-                        "ON books.authorId = authors.id",
+                        "ON books.author_id = authors.id",
                 (ResultSet rs, int rowNum) -> {
                     Book book = new Book();
                     book.setId(rs.getInt("id"));
-                    book.setAuthor(rs.getString("author"));
+                    book.setAuthor(rs.getString("authorFirstName")+" "+rs.getString("authorLastName"));
                     book.setTitle(rs.getString("title"));
-                    book.setPriceOld(rs.getInt("priceOld"));
+                    book.setPriceOld(rs.getInt("PRICE_OLD"));
                     book.setPrice(rs.getInt("price"));
                     return book;
                 });
@@ -42,7 +42,7 @@ public class BookService {
                 (ResultSet rs, int rowNum) -> {
                     Author author = new Author();
                     author.setId(rs.getInt("id"));
-                    author.setName(rs.getString("name"));
+                    author.setName(rs.getString("first_name")+" "+rs.getString("last_name"));
                     return author;
                 });
         return new ArrayList<>(authors);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,13 +3,14 @@ DROP TABLE IF EXISTS author;
 
 CREATE TABLE  PUBLIC.books(
                        id INT AUTO_INCREMENT PRIMARY KEY,
-                       authorId INT NOT NULL,
+                       author_id INT NOT NULL,
                        title VARCHAR(250) NOT NULL,
-                       priceOld  VARCHAR(250) DEFAULT NULL,
+                       PRICE_OLD  VARCHAR(250) DEFAULT NULL,
                        price VARCHAR(250) DEFAULT NULL
 );
 
 CREATE TABLE  PUBLIC.authors(
                         id INT AUTO_INCREMENT PRIMARY KEY,
-                        name VARCHAR(250) NOT NULL
+                        first_name VARCHAR(250) NOT NULL,
+                        last_name VARCHAR(250) NOT NULL
 );
